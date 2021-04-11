4b8ad77d93479a37d24f032ca5e297a8e79e739b
Eric <find.erics.email@gmail.com>
2021-04-10 22:09:47 -0400

    Pegasus Homepage

---
+++ Pegasus/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+const PORT = 8080
+
+
+app.use(express.static('static'))
+
+app.get(['/', '/index'], (req, res) => {
+	res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`)
+})
+
+
